9b5285b3b62ee7b1522a1d671f2a51ecb4450674
Eoin Hurrell <ultimatehurl@gmail.com>
2013-12-28 11:01:21 +0000

    Local setting file for Twitter Oauth.
    This file will be used for personal Oauth info, initial commit is empty
    (will never share my own).

---
+++ localSettings.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Settings file for Twitter Oauth access.
+
+Author: Eoin Hurrell <UltimateHurl@gmail.com>
+Created: 13-12-28
+
+"""
+
+OAUTH_KEY = ''
+OAUTH_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
